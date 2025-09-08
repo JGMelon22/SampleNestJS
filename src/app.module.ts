ab131ce4a28ca6a28d549ccbd71ad98706079fe6
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-09-08 13:22:03 -0300

    chore: setup TypeORM and database connection .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT!),
+      password: process.env.MYSQL_PASSWORD,
+      username: process.env.MYSQL_USER,
+      entities: [],
+      database: process.env.MYSQL_DATABASE,
+      synchronize: false,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
